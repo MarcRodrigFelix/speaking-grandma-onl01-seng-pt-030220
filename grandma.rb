a19ae467d7f7f5d944e138323c4b43db0a021f02
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-03 05:43:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,12 +16,10 @@
 
 def speak_to_grandma(string)
   
-  if string !== string.uppercase
-    return "HUH?! SPEAK UP, SONNY!"
+  if string == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.uppercase
-    return "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    "NO, NOT SINCE 1938!"
   end
   
 end
